e91f2286d29cedad4d7ee2ca2d895ab6beaf8244
Sergej Miroshnichenko <sergej.miroshnichenko.kr@gmail.com>
2023-03-16 03:12:13 +0200

    configured the alias and ESlint

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      components: '/src/components',
+      hooks: '/src/hooks',
+      assets: 'src/assets',
+      types: '/src/types',
+      routes: '/src/routes',
+      utils: '/src/utils',
+      store: '/src/store',
+      api: '/src/api'
+    }
+  }
 })
